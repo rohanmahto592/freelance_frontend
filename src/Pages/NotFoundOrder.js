3361465422530b7cf8ad4be7e7469b0c880cb18b
rohan <rohan.mahto@shl.com>
2023-08-20 23:39:39 +0530

    updated the ui of order

--- src/Pages/NotFoundOrder.js
+++ src/Pages/NotFoundOrder.js
@@ -9,11 +9,11 @@ const NotFoundOrder = () => {
           <div class="col-sm-12 ">
             <div class="col-sm-12 col-sm-offset-1  text-center">
               <div class="four_zero_four_bg">
-                <h1 class="text-center ">404</h1>
+              
               </div>
 
               <div class="contant_box_404">
-                <h3 class="h2">No order done by the user yet!!</h3>
+                <h3 class="h2">No orders placed by the user yet!!</h3>
               </div>
             </div>
           </div>
--- src/Pages/OrderStats/Order.js
+++ src/Pages/OrderStats/Order.js
@@ -6,110 +6,138 @@ const Order = () => {
   const type = sessionStorage.getItem("userType");
   const [userType, setUserType] = useState("");
   const [excelSheetInfo, setExcelSheetInfo] = useState(null);
-  const [userId, setuserId] = useState(0);
+  // const [userId, setuserId] = useState(0);
   const [users, setUsers] = useState(null);
+  const [UserInfo,setUserInfo]=useState(null);
   useEffect(() => {
     setUserType(type);
   }, [type]);
-  const fetchExcelSheetData = async(event) => {
+  const fetchExcelSheetData = async (event,id) => {
     event.preventDefault();
-    console.log(userId);
-    let Id=userId.split("  ")[1];
-    Id=Id.replace(/[()]/g, '');
-   const response= await  fetchExcelSheet(Id);
-      setExcelSheetInfo(response.data.message);
-
-  };
-  const fetchUserExcelData=async(id)=>{
-    const response= await  fetchExcelSheet(id);
+    const response = await fetchExcelSheet(id);
     setExcelSheetInfo(response.data.message);
-  }
+  };
+  const fetchUserExcelData = async (id) => {
+    const response = await fetchExcelSheet(id);
+    setExcelSheetInfo(response.data.message);
+  };
   useEffect(() => {
     sessionStorage.getItem("userType") === "ADMIN" &&
       fetchAllUsers().then((response) => {
+        console.log(response.data);
         if (response?.data?.success) {
+          setUserInfo(response?.data?.message)
           setUsers(response?.data?.message);
         }
       });
-    sessionStorage.getItem("userType")!=='ADMIN' && fetchUserExcelData(sessionStorage.getItem("id"));
+    sessionStorage.getItem("userType") !== "ADMIN" &&
+      fetchUserExcelData(sessionStorage.getItem("id"));
   }, []);
 
-  return (
-    <>
-      <div className="container mt-5">
-        {userType === "ADMIN" ? (
-          <h4 style={{ textAlign: "center" }}>All Orders</h4>
-        ) : (
-          <h4 style={{ textAlign: "center" }}>My Orders</h4>
-        )}
-        {userType === "ADMIN" && (
-         
-          <form onSubmit={fetchExcelSheetData}>
-             
-            <div className="row m-4">
-              <div className="col-sm-10 mt-2">
-                <input placeholder="Search order status by userId or username" onChange={(event)=>setuserId(event.target.value)} list="userlist" class="form-control" required />
-               <datalist id="userlist">
-                {users?.map((user,index)=>(
-                  <option key={index} value={user.firstName+" "+user.lastName+"  ("+user._id+")"}/>
-                ))}
-               </datalist>
-              </div>
-              <div className="col-sm-2 mt-2">
-                <button
-                  type="submit"
-                  className="btn btn-primary w-100"
-                >
-                 <i class="bi bi-search"></i>
+  function filterUser(username){
+    const users=UserInfo;
+    const filterUsers=users.filter((user)=>{
+      let word=user.firstName+user.lastName;
+      let email=user.email;
+      word=word.toLowerCase();
+     return word.includes(username.toLowerCase().trim()) || email.toLowerCase().includes(user);
+    })
+     
+    setUsers(filterUsers);
+  }
 
-                </button>
+  return (
+    <div className="container-fluid mt-5">
+      <div className="row m-4 shadow-sm bg-white rounded">
+        {userType === "ADMIN" && (
+          <div className="col-sm-3 mt-2">
+            <h4>Users</h4>
+            <hr />
+            <div className="shadow-sm bg-white p-2">
+              <form>
+                <input
+                 onChange={(event)=>filterUser(event.target.value)}
+                  placeholder="Search user"
+                  list="userlist"
+                  class="form-control"
+                  required
+                />
+                
+              </form>
+            </div>
+            <hr />
+            <div style={{ maxHeight: "400px",overflow:'auto' }}>
+              <ul class="list-group list-group mb-2">
+                {users?.map((user, index) => (
+                  <li
+                    key={index}
+                    class="list-group-item d-flex justify-content-between align-items-start "
+                  >
+                    <div class="ms-2 me-auto" onClick={(event)=>fetchExcelSheetData(event,user?._id)}>
+                      <div style={{fontFamily:'monospace'}} class="fw-bold">
+                        {user?.firstName + " " + user?.lastName}
+                      </div>
+
+                      <ul >
+                        <li style={{listStyleType:'none',fontFamily:'comic sans ms'}}> ⏩ {user?.email}</li>
+                      </ul>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        )}
+        {excelSheetInfo && excelSheetInfo?.length > 0?
+          <div className="col-sm-9 shadow-sm bg-white ">
+            {" "}
+            <div
+              style={{ maxHeight: "100vh", overflowY: "scroll" }}
+              className="row"
+            >
+              <div class="accordion" id="accordionExample">
+                {excelSheetInfo?.map((excelSheet, index) => (
+                  <div key={index} class="accordion-item m-2">
+                    <h2 class="accordion-header" id={`heading${index + 1}`}>
+                      <button
+                        class={`accordion-button ${
+                          index === 0 ? "" : "collapsed"
+                        }`}
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target={`#collapse${index + 1}`}
+                        aria-expanded={index === 0 ? "true" : "false"}
+                        aria-controls={`#collapse${index + 1}`}
+                        style={{
+                          backgroundColor: "slateblue",
+                          color: "white",
+                          fontWeight: "500",
+                          fontFamily: "sans-serif",
+                        }}
+                      >
+                        {excelSheet?.name}
+                      </button>
+                    </h2>
+                    <div
+                      id={`collapse${index + 1}`}
+                      class={`accordion-collapse collapse ${
+                        index === 0 ? "show" : ""
+                      }`}
+                      aria-labelledby="headingOne"
+                      data-bs-parent="#accordionExample"
+                    >
+                      <div class="accordion-body">
+                        <OrderTable excelRef={excelSheet?._id} />
+                      </div>
+                    </div>
+                  </div>
+                ))}
               </div>
             </div>
-          </form>
-        )}
-       {excelSheetInfo && excelSheetInfo?.length>0?<div
-          style={{ maxHeight: "100vh", overflowY: "scroll" }}
-          className="row"
-        >
-          <div class="accordion" id="accordionExample">
-            {excelSheetInfo?.map((excelSheet, index) => (
-              <div key={index} class="accordion-item m-2">
-                <h2 class="accordion-header" id={`heading${index + 1}`}>
-                  <button
-                    class={`accordion-button ${index === 0 ? "" : "collapsed"}`}
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target={`#collapse${index + 1}`}
-                    aria-expanded={index === 0 ? "true" : "false"}
-                    aria-controls={`#collapse${index + 1}`}
-                    style={{
-                      backgroundColor: "#EF715F",
-                      color: "white",
-                      fontWeight: "500",
-                      fontFamily: "sans-serif",
-                    }}
-                  >
-                    {excelSheet?.name}
-                  </button>
-                </h2>
-                <div
-                  id={`collapse${index + 1}`}
-                  class={`accordion-collapse collapse ${
-                    index === 0 ? "show" : ""
-                  }`}
-                  aria-labelledby="headingOne"
-                  data-bs-parent="#accordionExample"
-                >
-                  <div class="accordion-body">
-                    <OrderTable excelRef={excelSheet?._id} />
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>:<NotFoundOrder/>}
+          </div>:  <div className="col-sm-9 shadow-sm bg-white "><NotFoundOrder/></div>}
+      
       </div>
-    </>
+    </div>
   );
 };
 
