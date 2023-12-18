4145788aba3e8e4c3be41f0fece2d78c125cadd2
rohan <rohan.mahto@shl.com>
2023-12-18 16:24:37 +0530

    added image for about us page

--- src/Components/timeline/Timeline.js
+++ src/Components/timeline/Timeline.js
@@ -8,15 +8,15 @@ const Timeline = () => {
 <div>
       <section className="page-section" id="about">
             <div className="container">
-                <div className="text-center">
-                    <h4 className="section-heading text-uppercase">About Glimpse</h4>
-                </div>
                 <div className='row'>
                     {
                         AboutContent.map((item)=>(
                             
                             <div className='col-sm-6'>
-                                <div style={{minHeight:'270px',maxHeight:'270px',overflow:'auto',scrollbarWidth:'none',scrollBehavior:'smooth'}} className='col-sm-12 shadow-sm p-3 mb-5 bg-body rounded'>
+                                <div style={{minHeight:'400px',maxHeight:'270px',overflow:'auto',scrollbarWidth:'none',scrollBehavior:'smooth'}} className='col-sm-12 shadow-sm p-3 mb-5 bg-body rounded'>
+                                <div style={{height:'150px',width:'50%',margin:'auto'}}>
+                                        <img style={{width:'100%',height:"100%"}} src={item?.image} alt="knot"/>
+                                    </div>
                                     <div style={{height:'40px',width:'40px',margin:'auto'}}>
                                         <img style={{width:'100%',height:"100%"}} src={aboutus} alt="knot"/>
                                     </div>
--- src/Pages/About/AboutContent.js
+++ src/Pages/About/AboutContent.js
@@ -1,36 +1,51 @@
+import commitment from '../../Assets/Images/commitment.png'
+import specialization from '../../Assets/Images/speclization.png'
+import merchandize from '../../Assets/Images/merchandize.png'
+import team from '../../Assets/Images/dedicatedTeam.png'
+import expertize from '../../Assets/Images/expertize.png'
+import delivery from '../../Assets/Images/delivery.png'
+import value from '../../Assets/Images/value.png'
+import solutions from '../../Assets/Images/solutions.png'
 export const AboutContent=[
     {
+        image:specialization,
         title:"Specializes",
         desc:"Glimpse Education Private Limited specializes in providing comprehensive services in the field of direct print mail, merchandise for fairs, and welcome kits."
     },
     {
+        image:commitment,
         title:"Commitment",
         desc:"Their commitment to excellence and attention to detail has established them as a trusted partner for organizations seeking superior solutions for their promotional needs."
     },
     {
+        image:solutions,
         title:"Tailored solutions",
         desc:" We offer tailored solutions for designing, printing, and distributing promotional materials that cater to the unique requirements of each client."
     },
     {
+        image:merchandize,
         title:"Customized merchandise",
         desc:" Glimpse Education Private Limited provides customized merchandise for fairs and events that embodies their clients' brand identity, captivates their target audience, and leaves a lasting impression."
         
     },
     {
+        image:expertize,
         title:"Expertise",
         desc:" The company has expertise in managing welcome kit programs and has successfully partnered with esteemed institutions such as Saint Louis University to create exceptional welcome kits."
         
     },
     {
+        image:value,
         title:"Value",
         desc:" We value professionalism, innovation, and client satisfaction above all else and have forged strong partnerships with globally renowned organizations such as Globally Recruit, Globalshala, and Excelerate."
     },
     {
-
+        image:team,
         title:"Dedicated team",
         desc:"Glimpse Education Private Limited's dedicated team of experts is always ready to go the extra mile to ensure that clients' expectations are exceeded.",
     },
     {
+        image:delivery,
         title:"Timely delivery",
         desc:" Their commitment to excellence, tailored solutions, and timely delivery sets them apart in the industry and guarantees a remarkable experience for their clients."
     }
